bf7b6ded0eb44200a6a594212e2e8fe1d78bd419
jmathbr <joao.matheus92225@gmail.com>
2018-05-31 23:20:32 -0300

    3 finalizada

--- lista9/quest3.cpp
+++ lista9/quest3.cpp
@@ -14,6 +14,7 @@ struct Ticket{
 };
 int main(){
     Ticket ticket[max];
+    Ticket aux;
     int n;
     int diafixo,mesfixo;
     cout<<"Numero de Veiculos: ";
@@ -51,7 +52,23 @@ int main(){
 
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-
+            if(ticket[i].hora < ticket[j].hora){
+                aux = ticket[i];
+                ticket[i] = ticket[j];
+                ticket[j] = aux;
+            }
+            if(ticket[i].hora == ticket[j].hora){
+                if(ticket[i].minutos < ticket[j].minutos){
+                    aux = ticket[i];
+                    ticket[i] = ticket[j];
+                    ticket[j] = aux;
+                }
+            }
         }
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<"Carro: "<<i<<" Placa: "<<ticket[i].placa;
+        cout<<" Hora: "<<ticket[i].hora<<" Minutos: "<<ticket[i].minutos<<endl;
     }
 }
